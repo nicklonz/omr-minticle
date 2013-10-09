9a461f66aaa4ba62b6fe4215f20e36ad468d2424
Nick L <nicklonz@gmail.com>
2013-10-09 16:52:30 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MinTicle::Application.config.secret_key_base = '75434261c03c93577e6c29287f16fda62dcfb4506e207ec07900139ef7570790303baaf0659ff50826b61277291d202e9e391a87db357ebfab94d05553eab43a'
